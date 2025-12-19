ecb40dc13113f76ebe68894efe000f56d1c567b7
remi-code0806 <leo20190915@icloud.com>
2025-12-19 15:54:47 +0900

    Change objectFit to contain

--- app/page.tsx
+++ app/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
         style={{
           width: "100%",
           height: "100%",
-          objectFit: "cover",
+          objectFit: "contain",
           display: "block",
         }}
       />
